5361a7b1e51baf33178be87383f7f70c3003ba4d
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-10-30 21:10:33 -0300

    New

--- app/src/main/java/br/com/etecia/myapp/CadastrarActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CadastrarActivity.java
@@ -20,5 +20,9 @@ public class CadastrarActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
     }
 }
