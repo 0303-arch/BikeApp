3793557299b410a8f755397b08cf170f4025a3e2
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-11-27 20:21:20 -0300

    lista

--- app/src/main/java/br/com/etecia/myapp/Escuderia.java
+++ app/src/main/java/br/com/etecia/myapp/Escuderia.java
@@ -5,5 +5,24 @@ public class Escuderia {
     private String titulo;
     private int imgCar;
 
+    //criando construtor vazio
+    public Escuderia() {
+    }
 
+    //criando o getter and setter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImgCar() {
+        return imgCar;
+    }
+
+    public void setImgCar(int imgCar) {
+        this.imgCar = imgCar;
+    }
 }
